a84f5b31f40532d90d390f17f91ce5aec4f77996
Li Rui <31910376+lirui9825@users.noreply.github.com>
2021-09-13 13:12:19 +0800

    test

--- jd_bean_sign.js
+++ jd_bean_sign.js
@@ -117,7 +117,7 @@ async function downFile () {
 
 async function changeFile (content) {
   console.log(`开始替换变量`)
-  let newContent = content.replace(/var OtherKey = ``;/, `var OtherKey  = [{'cookie': '${cookie}'}];`);
+  let newContent = content.replace(/var OtherKey = ``;/, `var OtherKey  = '[{"cookie": "${cookie}""}]';`);
   console.log(newContent)
   if (process.env.JD_BEAN_STOP && process.env.JD_BEAN_STOP !== '0') {
     newContent = newContent.replace(/var stop = 0/, `var stop = ${process.env.JD_BEAN_STOP * 1}`);
